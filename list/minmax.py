d4bcf899c64d5c6d63bedda214690b46db47fbd6
smilemcm@gmail.com <smilemcm@gmail.com>
2020-07-27 15:35:50 +0900

    min max 알고리즘

---
+++ list/minmax.py
@@ -0,0 +1,13 @@
+import sys
+
+sys.stdin = open("input.txt","rt")
+T = int(input())
+
+for t in range(T):
+    n, m = map(int, input().split())
+    input_list = list(map(int, input().split()))
+    print(input_list.sort())
+    sorted_input_list = sorted(input_list)
+    min = sum(sorted_input_list[0:3])
+    max = sum(sorted_input_list[-1:-4:-1])
+    print("diff : ", max-min)
