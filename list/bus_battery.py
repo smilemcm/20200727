f8eafe99af02b3789bba04b7256a975dcfb958fd
smilemcm@gmail.com <smilemcm@gmail.com>
2020-08-02 21:55:40 +0900

    bus station

--- list/bus_battery.py
+++ list/bus_battery.py
@@ -1,4 +1,6 @@
 import sys
+from collections import deque
+
 
 sys.stdin = open("bus_battery.txt","rt")
 T = int(input())
@@ -7,16 +9,15 @@ for t in range(T):
     k, n, m = map(int, input().split())
     battery_charger_list = list(map(int, input().split()))
 
+
     bus_stations = [0] * (n)
     battery = k
     battery_refill_cnt = 0
 
     #### 모든 정거장에 충전소가 있던 없던 일단 0으로 세팅
-    for i in range(len(bus_stations) ):
-        for j in range(len(battery_charger_list)):
-            ###정거장에 충전소가 있으면 1로 세팅
-            if i == battery_charger_list[j]:
-                bus_stations[i] = 1
+    for i in range(len(bus_stations)):
+        if i in battery_charger_list:
+            bus_stations[i] = 1
 
 
     ### 여기서 부터는 실제 시뮬레이션
@@ -35,11 +36,11 @@ for t in range(T):
 
         if battery == 0 and bus_stations[i] == 0:
             print("목적지에 도착하지 못했습니다.")
-            battery_refill_cnt = 0
+
             break
 
     #print(t)
     print(t, battery_refill_cnt)
-
+    battery_refill_cnt = 0
 
 
