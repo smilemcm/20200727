8a574c05f6495396b5efa070772494713804742e
smilemcm@gmail.com <smilemcm@gmail.com>
2020-08-02 21:30:28 +0900

    bus station

--- list/bus_battery.py
+++ list/bus_battery.py
@@ -7,7 +7,7 @@ for t in range(T):
     k, n, m = map(int, input().split())
     battery_charger_list = list(map(int, input().split()))
 
-    bus_stations = [0] * (n+1)
+    bus_stations = [0] * (n)
     battery = k
     battery_refill_cnt = 0
 
@@ -20,17 +20,18 @@ for t in range(T):
 
 
     ### 여기서 부터는 실제 시뮬레이션
-    for i in range(len(bus_stations)):
+    for i in range(1,len(bus_stations)):
         battery = battery - 1
 
 
         ## 경우할 정류소에 배터리 충전소가 있을경우
-        if sum(bus_stations[i:i+battery])>0:
-            pass
+        if (sum(bus_stations[i+1 : i+1+battery])>0) or (n-1==i and battery > 0):
+            continue
         ### 이 버스 종류장에 충전소가 있으면 충전
         elif bus_stations[i] == 1:
             battery = k
             battery_refill_cnt = battery_refill_cnt + 1
+            continue
 
         if battery == 0 and bus_stations[i] == 0:
             print("목적지에 도착하지 못했습니다.")
